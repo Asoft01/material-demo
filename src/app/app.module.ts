fd8476310047f85b96afe373cec2c3b615b855fb
Adeleke Hammed Lekan <lekhad19@gmail.com>
2022-04-14 17:01:48 -0400

    Material Demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -8,8 +12,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule, 
+    // NoopAnimationsModule 
   ],
+  exports: [MatCheckboxModule],
   providers: [],
   bootstrap: [AppComponent]
 })
